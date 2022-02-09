0c919bd04fd3ef026eda58067e794e7ed509536b
nijuy <youjin6325@naver.com>
2022-02-09 21:53:54 +0900

    0209 추천목록 틀만 만들어둠 (작동x)

--- src/components/TdTemplate.js
+++ src/components/TdTemplate.js
@@ -5,7 +5,9 @@ function TdTemplate({ cc, uc, form, children }) {
     let month = new Date().getMonth() + 1;
     let day = new Date().getDate();
 
+    
     return(
+        <>
         <div className = "tdtemplate">
             <div className = "title">
                 <div className = "day">
@@ -26,6 +28,31 @@ function TdTemplate({ cc, uc, form, children }) {
                 오늘 할 일 {uc}개 중에서 {cc}개 완료 ! ᕕ( ᐛ )ᕗ
             </section>
         </div>
+        
+        <br></br>
+
+        <div className = "tdtemplate">
+            <div className = "suggestion_box">
+                <div className = "day">
+                    이런 일은 어떠세요?
+                </div>
+            </div>
+
+            <section className = "todo">
+                <div className = 'sg_choose'>
+                    오늘은  
+                    <button className = 'sg_button'> 집에만 있을래요 🏡</button>
+                    <button className = 'sg_button'> 외출하는 날! 😎 </button>
+                </div>
+                
+                <br/>
+
+                여기에 할 일 목록 추천을 띄워주는거지 근데 까먹어서 당장은 못해
+            </section>
+
+            <section className = "done"></section>
+        </div>
+        </>
     );
 }
 
